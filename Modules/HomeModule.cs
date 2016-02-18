2fef7d1665497534cc14ab141df567d0f2e7183d
Jay <Jaywhang83@gmail.com>
2016-02-18 14:47:36 -0800

    add more test for incorrect input

--- Modules/HomeModule.cs
+++ Modules/HomeModule.cs
@@ -8,8 +8,16 @@ namespace WeekdayFinderNS
   {
     public HomeModule()
     {
-      // Get["/"] = _ => View["index.cshtml"];
+      Get["/"] = _ => View["index.cshtml"];
+      Post["/result"] = _ =>
+      {
+        string month = Request.Form["month"];
+        string date = Request.Form["date"];
+        string year = Request.Form["year"];
 
+        WeekdayFinder day = new WeekdayFinder();
+        return View["result.cshtml", day.DayFinder(month, date, year)]; 
+      };
 
     }
   }
--- Tests/WeekdayFinderTest.cs
+++ Tests/WeekdayFinderTest.cs
@@ -19,5 +19,26 @@ namespace WeekdayFinderNS
       Assert.Equal("Not a valid input. Please enter a number only", test.DayFinder("2", "18", "fsdf"));
     }
 
+    [Fact]
+    public void DayFinder_fail2()
+    {
+      WeekdayFinder test = new WeekdayFinder();
+        Assert.Equal("Not a valid input. Please enter a number only", test.DayFinder("2", "{}", "2016"));
+    }
+
+    [Fact]
+    public void DayFinder_fail3()
+    {
+      WeekdayFinder test = new WeekdayFinder();
+        Assert.Equal("Not a valid input. Please enter a number only", test.DayFinder("23fds", "18", "2016"));
+    }
+    [Fact]
+    public void DayFinder_blank_space_fail()
+    {
+      WeekdayFinder test = new WeekdayFinder();
+        Assert.Equal("Not a valid input. Please enter a number only", test.DayFinder(" ", "18", " "));
+    }
+
+
   }
 }
